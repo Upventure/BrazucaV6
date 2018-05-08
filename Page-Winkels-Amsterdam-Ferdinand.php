6ec032da69274325a9e5db6fd56bc28a273014d3
Rens Boeser <0926072@hr.nl>
2018-05-08 09:32:01 +0200

    winkelpaginas update

--- Page-Winkels-Amsterdam-Ferdinand.php
+++ Page-Winkels-Amsterdam-Ferdinand.php
@@ -1,245 +1,49 @@
-
-<?php
-/* Template Name: Winkels Amsterdam Ferdinand */
-?>
-
-
 <?php get_header(); ?>
 
-   <div class="background-image2  background-image-home-top" style="">
-<div class="mask-layer mask-layer-home mask-layer-2">
-     <div class="content-hoofd-foto">
-
-        <h1 style="font-size:40px;">Brazuca Coffee </h1>
-        <!-- <div class="line-white"></div>
-       <p style="font-size:30px;">" Tangerina Sap"</p> -->
-
-
-
-
-
-
-       <p style="font-size:25px; margin-top:5px;" class="ondertitel"></p>
-       </div>
-
-     </div>
-       </div>
-
-       </div>
-<!--
-<div class="wrapper-top-section">
-<div class="advertisements">
-
-  <div class="section-white light">
-  <div class="flex wrapper-white-sections">
-  <div class="wrapper-white-section-section-50-50 reverse" style='align-content: space-between'>
-
-     <div class="Brazuca-logo-image">  <img class="" src="<?php bloginfo('template_url')?>/Images_Brazuca/Logo-Brazuca.png">      </div>
-
-             <div class="text-white-section" style="max-width:600px;">
-               <h1>Braziliaanse delicatessen / het beste uit Brazilië  </h1>
-               <p>
-Brazuca coffee brengt u het beste van Brazilie.
-<br>
-Naast onze bijzondere eigen gebrande koffie kunt u bij ons terecht voor vervrissende verse fruitsapen
-gemaakt van fruit uit de Amzone, serveren we autentieke Brazilaanse Acia bowl gerechten die je  gezondheid een boost geven
-en kunt u bij ons genieten van typische Brazilaanse lekernijnen zoals onze eigen gebaken casave broodjes.
-<br><br>
-Toewijding en passie dat is wat je proeft: kortom echte Brazliaanse ambacht!
-<br>
-   <br><br>
-
-</p>
-  <!-- </div> -->
-  </div>
-  </div>
-  </div>
-
-
-</div>
+<div class="section-1 image-ferdinand-1">
+    <div class="mask-layer mask-layer-home mask-layer-2">
+        <div class="content-hoofd-foto">
+            <h1 style="font-size:40px;">Brazuca Coffee </h1>
+            <!-- <div class="line-white"></div>
+            <p style="font-size:30px;">" Tangerina Sap"</p> -->
+            <p style="font-size:25px; margin-top:5px;" class="ondertitel"></p>
+        </div>
+    </div>
 </div>
 
+<div class="wrapper-top-section" style="border-top:  6px solid rgb(232, 237, 230);">
+    <div class="advertisements">
 
+        <div class="section-home-page light">
+            <div class="flex wrapper-white-sections">
+                <div class="wrapper-white-section-section-50-50 reverse" style='align-content: space-between'>
+                    <!-- <div class="Brazuca-logo-image">  <img class="" src="<?php bloginfo('template_url')?>/Images_Brazuca/Logo-Brazuca.png">      </div> -->
+                    <div class="text-white-section" style="">
+                        <h1>Braziliaanse delicatessen  </h1>
+                        <!-- <h1>Het beste uit Brazilië </h1> -->
+                        <p>
 
-<!-- <div class="wrapper-section-bottom" style="background-color:rgba();">
-<div class="advertisements">
-        <div class='referal-advertisement acai-full-section'>
-            <div class='content mask-layer' style="background-color:rgba(37, 35, 38, 0.36);">
-
-                    <div style='width: 100%; background-color:rgba(160, 141, 161, 0.29); height:75px;'>
-<h1 style="color:white; text-align:center;">  Açai Bowl</h1>
+                            Brazuca coffee brengt u het beste van Brazilie.
+                            <br>
+                            Naast onze bijzondere eigen gebrande koffie kunt u bij ons terecht voor vervrissende verse fruitsapen
+                            gemaakt van fruit uit de Amzone, serveren we autentieke Brazilaanse Acia bowl gerechten die je  gezondheid een boost geven
+                            en kunt u bij ons genieten van typische Brazilaanse lekernijnen zoals onze eigen gebaken casave broodjes.
+                            <br><br>
+                            Toewijding en passie dat is wat je proeft: kortom echte Brazliaanse ambacht!
+                            <br><br><br>
+                        </p>
                     </div>
-                                <div style='width: 100%; background-color:;'></div>
-                <div class='summary summary-extra' style="border: solid  2px whitesmoke">
-
-                  <p style="color:white">
-                    Açai bowl is nu razend populair. Een mooi opgemaakt kommetje boordevol vitamines. Acht jaar geleden maakten wij als éérste van Amsterdam deze overheerlijke açai bowl, overgevlogen uit Brazilië.
-
-                    Inmiddels zijn onze Açai Bowl gerechten een begrip geworden.
-                    In de meeste van onze winkels kunt het gerecht bestellen als ontbijt, lunch of maaltijd.
-          <br><br>
-                  De açaipalm groeit aan de oevers van de rivieren in de het Amazonegebied. Van de vruchten wordt een pulp gemaakt en ingevroren. Dit is de basis voor het gerecht, met muesli, vers fruit en guaraná wordt dit açaí na tigela (letterlijk açaí in de kom) genoemd. Het is een gerecht dat veel gegeten wordt door sporters, in Brazilië erg populair onder de surfers en wordt gewaardeerd om zijn stimulerende eigenschappen, vergelijkbaar met die in koffie of energiedranken. Açai helpt ook bij het verwijderen van afvalstoffen uit het lichaam.
-                  <br><br>
-                  Açai is bij ons ook verkrijgbaar als sap, op basis van appelsap of yoghurt.
-                  Onze açaí is 100% biologisch!
-                  Laat je verrassen door de unieke smaak van de açai, die bovendien ook erg goed voor de gezondheid is.
-
-
-
-    <br><br>
-
-
-
- </p>
-
-</div>
-
-                     <div style='width: 100%; background-color:rgba(160, 141, 161, 0.29); height:55px;'>
+                </div>
             </div>
         </div>
+    </div>
 
-      </div>
-    </div> -->
-
-
-
-
-<!--  -->
-
-<!-- <div class="advertisements">
-
- <div class="sectie-1-container" style="">
- <div class="sectie-1-container" style="">
-  <div class="sectie-1-colomn sectie-1-tekst paper-brown"  style="">
-
- <h1 style="">Oosprong Pão de queijo  </h1>
- <div class="line"></div>
- <p>
-
-
-                 Er gaan verhalen rond dat de eigenaren van de plantages de gewoonte hadden om 's ochtends brood te eten, maar het tarwemeel dat nodig was om brood te maken was niet zo gemakkelijk te vinden in Brazilië. Op dat moment produceerde Brazilië geen tarwe, dus moest dit geïmporteerd worden en dat was erg duur en bovendien niet altijd van goede kwaliteit. De koks moesten creatief zijn om toch brood te maken en het alternatief dat ze hadden was cassavemeel. Dit was wel voorradig en afkomstig uit eigen land. De cassavewortel werd al veel gegeten door Braziliaanse indianen, voor hen een belangrijk voedingsmiddel. Rond deze tijd was er een groot overschot aan kaas in Minas Gerais en om dit niet te verspillen, werd de al ouder geworden kaas geraspt en aan het deeg toegevoegd. Dit is het verhaal van de beroemde pão de queijo.
-<br><br>
-                 Onze pão de queijo is gemaakt op basis van tapiocameel en daardoor is het glutenvrij en het wordt dagelijks vers gebakken. Afkomstig uit een goed bewaard recept. Samen met onze romige cappuccino vormt dit een overheerlijk duo. Kom langs en ervaar het zelf!
-
-   <br><br>
-
-
-
- </p>
- </div>
-
-
- <div class="sectie-1-image-section schrink sectie-1-background-image bites-section-half">
- </div>
-
-
-
- </div>
- </div>
-
-</div> -->
-
-
-</div>
-   <!-- <div class="background-image2 back-home slider-coffee background-image-Tagerina" style="height:500px">
-<div class="mask-layer mask-layer-2 mask-layer-fruit-page">
-     <div class="content-hoofd-foto">
-
-        <h1 style="font-size:40px;">Brazuca fruitsappen </h1>
-
-       <p style="font-size:25px; margin-top:5px;" class="ondertitel">Mandarijnsap</p>
-       </div>
-
-     </div>
-       </div>
-
-       </div> -->
-
-
-<!-- --------------------------------- sectie voor te tabellen----------------------------------->
-
-
-<!-- <div class="advertisements">
-
- <div class="sectie-1-container" style="">
- <div class="sectie-1-container" style="">
-
-  <div class="sectie-1-colomn sectie-1-tekst paper-brown dark-green"  style="max-width:350px;">
-
- <h1 style="">Inhoud fruitsap</h1>
- <div class="line"></div>
- <p>
-
-   Vitamine A en C functioneren als antioxidanten, maar spelen ook een belangrijke rol in de algemene groei en ontwikkeling en het versterken van het immuunsysteem. Calcium is belangrijk voor het behoud van gezonde tanden en botten en magnesium speelt een essentiële rol in cellulaire energie productie. Kalium is een zeer belangrijk mineraal voor een goede werking van cellen, weefsels en organen in het menselijk lichaam. Daarnaast is het cruciaal in het reguleren van het hartritme en de werking van de spieren.
-
-   <br><br>
-
- </p>
- </div>
- <div class="sectie-1-image-section schrink sectie-1-background-image Tagerina-coffee-section-4">
- </div>
-
- </div>
- </div>
-
-</div>
-     </div>
-
-</div> -->
-
-
-
-<div class="wrapper-top-section" style="border-top:  6px solid rgb(232, 237, 230);">
-<div class="advertisements">
-
-  <div class="section-home-page light">
-  <div class="flex wrapper-white-sections">
-  <div class="wrapper-white-section-section-50-50 reverse" style='align-content: space-between'>
-
-     <!-- <div class="Brazuca-logo-image">  <img class="" src="<?php bloginfo('template_url')?>/Images_Brazuca/Logo-Brazuca.png">      </div> -->
-
-             <div class="text-white-section" style="">
-               <h1>Braziliaanse delicatessen  </h1>
-   <!-- <h1>Het beste uit Brazilië </h1> -->
-               <p>
-
-Brazuca coffee brengt u het beste van Brazilie.
-<br>
-Naast onze bijzondere eigen gebrande koffie kunt u bij ons terecht voor vervrissende verse fruitsapen
-gemaakt van fruit uit de Amzone, serveren we autentieke Brazilaanse Acia bowl gerechten die je  gezondheid een boost geven
-en kunt u bij ons genieten van typische Brazilaanse lekernijnen zoals onze eigen gebaken casave broodjes.
-<br><br>
-Toewijding en passie dat is wat je proeft: kortom echte Brazliaanse ambacht!
-
-
-<br>
-
-
-
-
-   <br><br>
-
-</p>
-  <!-- </div> -->
-  </div>
-  </div>
-  </div>
-
-
-</div>
-</div>
-
-
-
-
-
-<div class="wrapper-top-section" style="border-top:  6px solid rgb(232, 237, 230);">
     <div class="advertisements">
         <div class="section-home-page light">
             <div class="flex wrapper-white-sections">
                 <div class="wrapper-white-section-section-50-50 home-section">
                     <!-- <div class="image-white-section"></div>       -->
-                         <div class="section-2 image-coffee-2"></div>
+                    <div class="section-2 image-coffee-2"></div>
                     <div class="text-white-section">
 
                         <h1>Brazuca coffee</h1>
@@ -249,54 +53,48 @@ Toewijding en passie dat is wat je proeft: kortom echte Brazliaanse ambacht!
                             Iedere oogst weer bepalen wij de beste combinatie van koffiebonen voor samenstellen van onze malanges door de verschillende bonen te proefen en  Coffee farms in Brazilie te bezoeken. Door onze jarenlange ervaring en kennis van kofiebonen zijn wij in staat gebleken om hoogwaardige Brazaliaanse koffie naar Nederland te halen.
                             <br><br><br>          <br><br>  <br>
                             <a class="button-standard" style="padding: 5px 75px 5px 75px; text-decoration: underline" href="localhost/testpage">Brazuca koffie</a>
-
-
-
                         </p>
-
                     </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-
-
-
-
-<div class="advertisements">
-    <div class="light section-home-page">
-        <div class="flex wrapper-white-sections">
-            <div class="wrapper-white-section-section-50-50 reverse">
-                <div class="section-2 image-açaibowl-2"></div>
-                <div class="text-white-section">
-                    <h1>Açai Bowl</h1>
-                    <p>
-                        Açai bowl is nu razend populair. Een mooi opgemaakt kommetje boordevol vitamines. Acht jaar geleden maakten wij als éérste van Amsterdam deze overheerlijke açai bowl, overgevlogen uit Brazilië.
-                        <br><br>
-                        Inmiddels zijn onze Açai Bowl gerechten een begrip geworden.
-                        In de meeste van onze winkels kunt het bestellen als ontbijt lunch of maaltijd.
-                        <!-- en kan het als  als ontbijt , lunch of als maaltijd worden besteld in de meeste van onze winkels. -->
-                        <br><br><br>          <br><br>  <br>
-                              <a class="button-standard" style="padding: 5px 75px 5px 75px; text-decoration: underline" href="localhost/testpage">Acai bowl gerechten</a>
-                    </p>
                 </div>
             </div>
         </div>
     </div>
 </div>
 
-
-
-
-
-
-
-
-
+<div class="background-image2  background-image-home-center" style="">
+    <div class="mask-layer mask-layer-home mask-layer-2">
+        <div class="content-hoofd-foto">
+            <h1 style="font-size:40px;">Brazuca Coffee </h1>
+            <!-- <div class="line-white"></div>
+			<p style="font-size:30px;">" Tangerina Sap"</p> -->
+            <p style="font-size:25px; margin-top:5px;" class="ondertitel"></p>
+        </div>
+    </div>
+</div>
 
 <div class="wrapper-top-section">
+    <div class="advertisements">
+        <div class="light section-home-page">
+            <div class="flex wrapper-white-sections">
+                <div class="wrapper-white-section-section-50-50 reverse">
+                    <div class="section-2 image-açaibowl-2"></div>
+                    <div class="text-white-section">
+                        <h1>Açai Bowl</h1>
+                        <p>
+                            Açai bowl is nu razend populair. Een mooi opgemaakt kommetje boordevol vitamines. Acht jaar geleden maakten wij als éérste van Amsterdam deze overheerlijke açai bowl, overgevlogen uit Brazilië.
+                            <br><br>
+                            Inmiddels zijn onze Açai Bowl gerechten een begrip geworden.
+                            In de meeste van onze winkels kunt het bestellen als ontbijt lunch of maaltijd.
+                            <!-- en kan het als  als ontbijt , lunch of als maaltijd worden besteld in de meeste van onze winkels. -->
+                            <br><br><br>          <br><br>  <br>
+                            <a class="button-standard" style="padding: 5px 75px 5px 75px; text-decoration: underline" href="localhost/testpage">Acai bowl gerechten</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div class="advertisements">
         <div class="section-home-page section-white light">
             <div class="flex wrapper-white-sections">
@@ -309,8 +107,8 @@ Toewijding en passie dat is wat je proeft: kortom echte Brazliaanse ambacht!
                             Cassavemeel wordt ook wel tapiocameel genoemd.
                             <br><br>
                             Er wordt beweerd dat het recept al bestaat sinds de achttiende eeuw, maar het broodje werd pas populair in Brazilië in de jaren 50. De pão de queijo is een van de bekendste gerechten uit de Braziliaanse keuken. Er is geen Braziliaan die de pão de queijo niet kent, en geen buitenlander die niet verliefd wordt bij de eerste hap.
-                                    <br><br>  <br>          <br><br>  <br>
-                              <a class="button-standard" style="padding: 5px 75px 5px 75px; text-decoration: underline" href="localhost/testpage">Brazuca Bites</a>
+                            <br><br>  <br>          <br><br>  <br>
+                            <a class="button-standard" style="padding: 5px 75px 5px 75px; text-decoration: underline" href="localhost/testpage">Brazuca Bites</a>
 
                         </p>
                     </div>
@@ -320,6 +118,5 @@ Toewijding en passie dat is wat je proeft: kortom echte Brazliaanse ambacht!
     </div>
 </div>
 
-</div>
-    <?php get_template_part('footer', 'contact'); get_footer_contact('Ferdinand bolstraat') ?>
+<?php get_template_part('footer', 'contact'); get_footer_contact('Ferdinand Bolstraat'); ?>
 <?php get_footer(); ?>
