9366776f61710298e25b131c67f9bb34e18fee2b
Jeroen <j.goemans@upventure.nl>
2018-05-12 00:01:35 +0200

    Font size aanpassingen

---
+++ Page-over-ons-nieuw.php
@@ -0,0 +1,141 @@
+<?php
+/* Template Name:  Over ons new*/
+?>
+
+<?php get_header(); ?>
+
+<div class="section-1 image-home-1">
+    <div class="mask-layer mask-layer-home mask-layer-2">
+        <div class="content-hoofd-foto">
+            <img class="" style="max-width:240px; margin-top:40px; padding:0px;"  src="<?php bloginfo('template_url')?>/Images_Brazuca/Brazuca-white.smal.png">
+            <p style="font-size:20px; margin-top:5px;" class="ondertitel">
+                Echte vers gebrande koffie en authentieke Braziliaanse delicatessen 
+                <br>
+                proeft u bij Brazuca
+            </p>
+        </div>
+    </div>
+</div>
+
+<d
+<div class="section-1 image-winkels-1" style="">
+    <div class="mask-layer mask-layer-home mask-layer-2">
+
+
+      <div class="content-hoofd-foto">
+          <!-- <div class="line-white"></div>
+          <p style="font-size:30px;">" Tangerina Sap"</p> -->
+      <!-- <h1 style="font-size:30px; margin-top:20px; padding:0px;">Brazuca Coffee </h1> -->
+          <img class="" style="max-width:240px; margin-top:80px; padding:0px;"  src="<?php bloginfo('template_url')?>/Images_Brazuca/Brazuca-white.smal.png">
+        <h1 style='font-size:30px;'> <?php wp_title(); ?>  </h1>
+
+  <p style="font-size:20px; margin-top: 5px;" class="ondertitel">Bezoek de brazuca winkel <br>bij u in de buurt</p>
+
+
+      </div>
+
+
+
+
+        <div class="content-hoofd-foto">
+
+            <!-- <div class="line-white"></div>
+            <p style="font-size:30px;">" Tangerina Sap"</p> -->
+
+        </div>
+    </div>
+</div>
+
+<div class="wrapper-top-section" style="border-top: 6px solid rgb(232, 237, 230);">
+    <div class="advertisements-winkels">
+        <div class="section-winkel-page light winkel-achtergrond" style="padding: 0">
+            <div style="background: rgba(255, 255, 255, 0.90); padding-top: 40px; padding-bottom: 20px;">
+                <div class="flex wrappper-winkel-sections">
+                    <div class="wrapper-white-section-section-50-50 home-section">
+                        <!-- <div class="image-white-section"></div> -->
+
+                        <!-- MULTIPLE ITEMS TESTING -->
+
+                        <div class="referal-advertisement-div-winkels">
+
+                            <div class="winkels-overzicht-header">
+                                <h1>De winkels van Brazuca</h1>
+                                <p>Selecteer een winkel voor meer informatie</p>
+                            </div>
+
+                            <div class="multiple-items-standard">
+                                <div class="item-standard-border">
+                                    <a href="localhost/rijnstraat">
+                                        <h2>Amsterdam Rijnstraat</h2>
+                                        <div class="image-standard">
+                                            <img src="<?php bloginfo('template_url')?>/Images_Brazuca/Brazuca-rotterdam-tablet.jpg">
+                                        </div>
+                                        <div class="content-standard" style="padding: 9px 9px;">
+                                            <!-- <h4 style="text-align: center; margin-top: 5px;">Rijnstraat 22, Amsterdam</h4> -->
+                                            <p>
+                                              Brazuca Coffee Ferdinand Bolstraat<br>
+                                             Ferdinand Bolstraat 113 H <br>
+                                             1072 LE Amsterdam<br>
+                                            </p>
+                                        </div>
+                                        <div class="flex-active">
+                                            <a class="button-standard" href="localhost/rijnstraat">Naar deze vestiging</a>
+                                        </div>
+                                        <br>
+                                    </a>
+                                </div>
+
+                                <div class="item-standard-border">
+                                    <a href="localhost/rijnstraat">
+                                        <h2>Amsterdam Ferdinand bolstraat</h2>
+                                        <div class="image-standard">
+                                            <img src="<?php bloginfo('template_url')?>/Images_Brazuca/Brazuca-rotterdam-tablet.jpg">
+                                        </div>
+                                        <div class="content-standard" style="padding: 9px 9px;">
+                                            <!-- <h4 style="text-align: center; margin-top: 5px;">Ferdinand Bolstraat 113 H, Amsterdam</h4> -->
+                                            <p>
+                                                Brazuca Coffee Ferdinand Bolstraat<br>
+                                                Ferdinand Bolstraat 113 H <br>
+                                                1072 LE Amsterdam<br>
+                                            </p>
+                                        </div>
+                                        <div class="flex-active">
+                                            <a class="button-standard" href="localhost/ferdinand-bolstraat">Naar deze vestiging</a>
+                                        </div>
+                                        <br>
+                                    </a>
+                                </div>
+
+                                <div class="item-standard-border">
+                                    <a href="localhost/rijnstraat">
+                                        <h2>Rotterdam Markthal</h2>
+                                        <div class="image-standard">
+                                            <!-- <img src="../wp-content/themes/Brazuca/Images_Brazuca/879-2.JPG"> -->
+                                            <img src="<?php bloginfo('template_url')?>/Images_Brazuca/Brazuca-rotterdam-tablet.jpg">
+                                        </div>
+                                        <div class="content-standard" style="padding: 9px 9px;">
+                                            <!-- <h4 style="text-align: center; margin-top: 5px;">Markthal, Rotterdam</h4> -->
+                                            <p>
+                                                Brazuca Coffee Ferdinand Bolstraat<br>
+                                                Ferdinand Bolstraat 113 H <br>
+                                                1072 LE Amsterdam<br>
+                                            </p>
+                                        </div>
+                                        <div class="flex-active">
+                                            <a class="button-standard" href="localhost/markthal">Naar deze vestiging</a>
+                                        </div>
+                                        <br>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</div>
+
+<?php get_footer(); ?>
