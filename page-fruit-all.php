3770b16e8cf5f4347583d31123cb0be45d97a0ec
Rens Boeser <0926072@hr.nl>
2018-05-13 19:10:31 +0200

    fruitsappen links

--- page-fruit-all.php
+++ page-fruit-all.php
@@ -6,21 +6,15 @@
 
 <div class="section-1 image-fruitsappen-1">
     <div class="mask-layer mask-layer-2 mask-layer-fruit-page">
-      <div class="content-hoofd-foto">
-          <!-- <div class="line-white"></div>
-          <p style="font-size:30px;">" Tangerina Sap"</p> -->
-      <!-- <h1 style="font-size:30px; margin-top:20px; padding:0px;">Brazuca Coffee </h1> -->
-          <img class="" style="max-width:240px; margin-top:80px; padding:0px;"  src="<?php bloginfo('template_url')?>/Images_Brazuca/Brazuca-white.smal.png">
-        <h1 style='font-size:35px;'>  Verse fruitsappen </h1>
-
-  <p style="font-size:20px; margin-top: 5px;" class="ondertitel">Heerlijke verse fruitsappen <br>rechtstreeks uit de Amazone</p>
-
-
-      </div>
+        <div class="content-hoofd-foto">
+            <img class="" style="max-width:240px; margin-top:80px; padding:0px;"  src="<?php bloginfo('template_url')?>/Images_Brazuca/Brazuca-white.smal.png">
+            <h1 style='font-size:35px;'>  Verse fruitsappen </h1>
+            <p style="font-size:20px; margin-top: 5px;" class="ondertitel">
+                Heerlijke verse fruitsappen <br>rechtstreeks uit de Amazone
+            </p>
+        </div>
     </div>
 </div>
-
-
 
 
 <div class="wrapper-fruit-all">
@@ -35,11 +29,14 @@
                 In ons sapjes assortiment hebben wij de steeds populair wordende Açai (Braziliaanse palmbes), maar ookCaju (cashew fruit), Abacaxi com hortelã (ananas met munt), Maracujá (passievrucht), Manga (mango), Tangerina (mandarijn) en Acerola (Barbados bes). Meer informatie over de voedingswaarden en de gezondheidsvoordelen van elk fruitsoort kunt u vinden op deze pagina.
             </p>
         </div>
+
         <div>
             <div class="container-div container-div-thumbnails" style="background-color:white">
 
                 <div class="colomn colomn-img column-3">
-                    <img src="<?php bloginfo('template_url')?>/Images_Brazuca/Acai-side.jpg">
+                    <a href="<?php bloginfo('template_url') ?>/acai-bessen-sap">
+                        <img src="<?php bloginfo('template_url')?>/Images_Brazuca/Acai-side.jpg">
+                    </a>
                     <div class="foto-blok">
                         <h3>Açai sap</h3>
                     </div>
@@ -47,14 +44,18 @@
 
 
                 <div class="colomn colomn-img column-3">
-                    <img src="<?php bloginfo('template_url')?>/Images_Brazuca/ananas-side.jpg">
+                    <a href="<?php bloginfo('template_url') ?>/ananas-sap">
+                        <img src="<?php bloginfo('template_url')?>/Images_Brazuca/ananas-side.jpg">
+                    </a>
                     <div class="foto-blok">
                         <h3>Ananas sap</h3>
                     </div>
                 </div>
 
                 <div class="colomn colomn-img column-3">
-                    <img src="<?php bloginfo('template_url')?>/Images_Brazuca/Acerola-side.jpg">   </img>
+                    <a href="<?php bloginfo('template_url') ?>/acerola-sap">
+                        <img src="<?php bloginfo('template_url')?>/Images_Brazuca/Acerola-side.jpg">
+                    </a>
                     <div class="foto-blok">
                         <h3>Acerola sap</h3>
                     </div>
@@ -63,7 +64,9 @@
             <div class="container-div container-div-thumbnails" style="background-color:white; max-width:100%;">
 
                 <div class="colomn colomn-img column-3">
-                    <img src="<?php bloginfo('template_url')?>/Images_Brazuca/Mango-side.jpg">
+                    <a href="<?php bloginfo('template_url') ?>/mango-sap">
+                        <img src="<?php bloginfo('template_url')?>/Images_Brazuca/Mango-side.jpg">
+                    </a>
                     <div class="foto-blok">
                         <h3>Mango sap</h3>
                     </div>
@@ -71,7 +74,9 @@
 
 
                 <div class="colomn colomn-img column-3">
-                    <img src="<?php bloginfo('template_url')?>/Images_Brazuca/Passievrucht-side.jpg">
+                    <a href="<?php bloginfo('template_url') ?>/passievrucht-sap">
+                        <img src="<?php bloginfo('template_url')?>/Images_Brazuca/Passievrucht-side.jpg">
+                    </a>
                     <div class="foto-blok">
                         <h3> Passievrucht sap</h3>
                     </div>
@@ -83,75 +88,4 @@
     </div>
 </div>
 
-
-<!--
-<div class="wrapper-fruit-all">
-  <div class="mask-layer mask-layer-home mask-layer-2">
-
-
-    <div class="advertisements" style="background-color: white; opacity:50%;">
-
-
-
-             <h1 style="text-align:center; padding:50px 10px; font-size: 40px;"  class="Mondfit-Heading1"> Brazuca fruitsappen</h1>
-
-
-                                                                                 <div class="container-div container-div-thumbnails">
-
-                                                                                 <div class="colomn colomn-img column-3">
-                                                                               <img src="<?php bloginfo('template_url')?>/Images_Brazuca/AdobeStock_28930302_Preview.jpeg">   </img>
-                                                                                   <div class="foto-blok">
-                                                                                        <h3>Tagerina</h3>
-                                                                                    </div>
-                                                                                 </div>
-
-
-                                                                                 <div class="colomn colomn-img column-3">
-                                                                       <img src="<?php bloginfo('template_url')?>/Images_Brazuca/AdobeStock_28930302_Preview.jpeg">   </img>
-                                                                                   <div class="foto-blok">
-                                                                                     <h3>Volwassenen</h3>
-                                                                                   </div>
-                                                                                 </div>
-
-                                                                                 <div class="colomn colomn-img column-3">
-                                                                               <img src="<?php bloginfo('template_url')?>/Images_Brazuca/AdobeStock_28930302_Preview.jpeg">   </img>
-                                                                                   <div class="foto-blok">
-                                                                                     <h3>Ouderen</h3>
-                                                                                   </div>
-                                                                                 </div>
-
-
-
-             </div>
-                   <div class="container-div container-div-thumbnails">
-
-             <div class="colomn colomn-img column-3">
-         <img src="<?php bloginfo('template_url')?>/Images_Brazuca/AdobeStock_28930302_Preview.jpeg">   </img>
-                 <div class="foto-blok">
-                   <h3>Babby's en kleuters</h3>
-                     </div>
-             </div>
-
-
-             <div class="colomn colomn-img column-3">
-     <img src="<?php bloginfo('template_url')?>/Images_Brazuca/AdobeStock_28930302_Preview.jpeg">   </img>
-               <div class="foto-blok">
-                 <h3>Hart - en Vaatziekte</h3>
-               </div>
-             </div>
-
-
-
-             <div class="blue-stripe-line h100"></div>
-
-
-    </div>
-
-    </div>
-    </div>
-
-
-
-</div>
-</div> -->
 <?php get_footer(); ?>
